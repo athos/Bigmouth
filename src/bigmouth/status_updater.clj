a44cca8ea0921ad827e16b6002cd0253b6bf5fff
OHTA Shogo <athos0220@gmail.com>
2017-04-25 18:09:34 +0900

    Publish Atom feed to subscribers via status updater

--- src/bigmouth/status_updater.clj
+++ src/bigmouth/status_updater.clj
@@ -1,5 +1,6 @@
 (ns bigmouth.status-updater
-  (:require [bigmouth.protocols :as proto]
+  (:require [bigmouth.atom :as atom]
+            [bigmouth.protocols :as proto]
             [bigmouth.utils :as utils]
             [org.httpkit.client :as http]
             [pandect.algo.sha1 :as sha1]))
@@ -9,10 +10,11 @@
 (defn make-status-updater [subscription-repo configs]
   (->StatusUpdater subscription-repo configs))
 
-(defn update! [this account content]
-  (let [subscription-repo (:subscription-repo this)]
+(defn update! [this account entry]
+  (let [subscription-repo (:subscription-repo this)
+        atom-feed (atom/atom-feed account [entry] (:configs this))]
     (doseq [subs (proto/find-subscriptions subscription-repo account)
-            :let [sig (sha1/sha1-hmac content (:secret subs))]]
+            :let [sig (sha1/sha1-hmac atom-feed (:secret subs))]]
       (http/post (:callback subs)
                  {:headers {"X-Hub-Signature" (str "sha1=" sig)}
-                  :body content}))))
+                  :body atom-feed}))))
