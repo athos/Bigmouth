83b7df607377a8611b5edec3f5f236276ac4a575
OHTA Shogo <athos0220@gmail.com>
2017-04-28 14:01:34 +0900

    Look up account before updating status

--- src/bigmouth/status_updater.clj
+++ src/bigmouth/status_updater.clj
@@ -1,19 +1,22 @@
 (ns bigmouth.status-updater
   (:require [bigmouth.atom :as atom]
+            [bigmouth.models.account :as account]
             [bigmouth.models.subscription :as subs]
             [bigmouth.utils :as utils]
             [org.httpkit.client :as http]
             [pandect.algo.sha1 :as sha1]))
 
-(defrecord StatusUpdater [subscription-repo configs])
+(defrecord StatusUpdater [account-repo subscription-repo configs])
 
-(defn make-status-updater [subscription-repo configs]
-  (->StatusUpdater subscription-repo configs))
+(defn make-status-updater [account-repo subscription-repo configs]
+  (->StatusUpdater account-repo subscription-repo configs))
 
-(defn update! [this account entry]
-  (let [subscription-repo (:subscription-repo this)
+(defn update! [this username entry]
+  (let [account-repo (:account-repo this)
+        subscription-repo (:subscription-repo this)
+        account (account/find-account account-repo username)
         atom-feed (atom/atom-feed account [entry] (:configs this))]
-    (doseq [subs (subs/find-subscriptions subscription-repo account)
+    (doseq [subs (subs/find-subscriptions subscription-repo username)
             :let [sig (sha1/sha1-hmac atom-feed (:secret subs))]]
       (http/post (:callback subs)
                  {:headers {"X-Hub-Signature" (str "sha1=" sig)}
